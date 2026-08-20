1b6e4c43dcbe27fa0bf3b4a348803d39e29bc437
salmaa-tamer <salmatamerramzy307@gmail.com>
2026-08-20 19:03:15 +0300

    finished coding task

--- Coding_Assignment/Question.py
+++ Coding_Assignment/Question.py
@@ -1,15 +1,30 @@
 def monitor_following_distance(distances: list[float], speeds: list[float]) -> tuple[int, float, int]:
-    """
-    Analyzes following distance compared to safe distance (speed * 0.5).
-    
-    Args:
-        distances (list[float]): Distance to the lead car at each second.
-        speeds (list[float]): Speed of our car at each second.
-        
-    Returns:
-        tuple[int, float, int]: (tailgating_seconds, minimum_distance, tailgate_incidents)
-            - tailgating_seconds: total seconds distance was < safe distance
-            - minimum_distance: absolute closest distance to the lead car (return 0.0 if empty list)
-            - tailgate_incidents: number of separate instances the car started tailgating
-    """
-    pass
+    incident_counter = 0
+    total_tailgating_secs = 0
+    tailgating_now = False  #no actual use, helps me track state
+    Running_incident = False
+    if len(distances) ==0 :
+        return (0, 0.0 ,0)
+    else:
+        min_distance = distances [0]
+        for i in range(len(distances)):
+            my_distance = distances [i]
+            my_speed = speeds [i]
+            safe_distance = my_speed* 0.5
+            if my_distance < min_distance:
+                min_distance = my_distance
+            if my_distance >= safe_distance:
+                tailgating_now = False
+                Running_incident = False
+            else:
+                tailgating_now = True
+                if Running_incident == True:
+                    total_tailgating_secs = total_tailgating_secs +1
+                else:
+                    Running_incident = True
+                    incident_counter = incident_counter +1
+                    total_tailgating_secs = total_tailgating_secs +1
+    return (total_tailgating_secs, min_distance,incident_counter)
+
+
+
